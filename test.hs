f0204221804d3adb9c28c441b535275fe507cca6
Jan <jan@parseclabs.org>
2020-03-06 15:09:25 +0100

    Add homomorphic encryption and some clean up.

--- test.hs
+++ test.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
--- have to set manually with :set -XTypeApplications
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -12,13 +11,18 @@ import Test.QuickCheck
 import Data.List
 import qualified Data.Map.Strict as Map
 
+
+
+
+------------------------------------Abstract algebra ------------------------
+
 class Monoid a where
   add :: a -> a -> a
   addId :: a
 
 class Monoid a => Group a where
   addInv :: a -> a
-
+  
 -- Actually it has to be an ABELIAN group
 class Group a => Ring a where
   mul :: a -> a -> a
@@ -28,9 +32,8 @@ class Group a => Ring a where
 class Ring a => Field a where
   mulInv :: a -> a
 
--- To get a field you need this exstra
--- commutative of mul
--- mul inverse
+sub :: Group a => a -> a -> a
+sub p1 p2 = add p1 (addInv p2)
 
 monoidIsAssociative :: (Monoid m, Eq m) => m -> m -> m -> Bool
 monoidIsAssociative a b c = add a (add b c) == add (add a b) c
@@ -97,26 +100,30 @@ isField = do
   isCommutativeRing @f
   quickCheck $ fieldHasInverse @f
 
-instance Monoid Int where
+---------------------------------- Integer insatnces ---------------------
+
+instance Monoid Integer where
   add = (+)
   addId = 0
 
-instance Group Int where
+instance Group Integer where
   addInv a = -a
 
-instance Ring Int where
+instance Ring Integer where
   mul = (*)
   mulId = 1
 
-data Poly a = Poly (Map.Map Int a)
+----------------------------------- Polynomials --------------------------
+
+data Poly a = Poly (Map.Map Integer a)
   deriving (Show)
 
-degree :: Poly a -> Int
+degree :: Poly a -> Integer
 degree (Poly m)
   | Map.null m = 0
   | otherwise  = maximum . Map.keys $ m
 
-coeff :: Ring a => Poly a -> Int -> a
+coeff :: Ring a => Poly a -> Integer -> a
 coeff (Poly m) k = Map.findWithDefault addId k m
 
 instance (Eq a, Ring a) => Eq (Poly a) where
@@ -136,13 +143,12 @@ instance Ring a => Monoid (Poly a) where
     where maxDegree = max (degree p1) (degree p2)
           coeffs    = Map.fromList [(i, j) | i <- [0 .. maxDegree],
                                     let j = add (coeff p1 i) (coeff p2 i)]
-    
   addId = Poly Map.empty
 
 instance Ring a => Group (Poly a) where
   addInv (Poly m) = Poly (Map.map (\e -> addInv e) m)
 
-mulCoeff :: Ring a => Poly a -> Poly a -> Int -> a
+mulCoeff :: Ring a => Poly a -> Poly a -> Integer -> a
 mulCoeff p1 p2 i = foldl (\acc -> \ele -> add acc ele) addId $ (\(a, b) -> mul a b) <$> getList p1 p2 i
   where getList p1 p2 i = (\(a,b) -> (coeff p1 a, coeff p2 b)) <$> [(a, i - a) | a <- [0 .. i]]
 
@@ -153,7 +159,7 @@ instance Ring a => Ring (Poly a) where
                                     let j = mulCoeff p1 p2 i]
   mulId     = Poly (Map.fromList [(0, mulId)])
 
-expo :: Ring a => a -> Int -> a
+expo :: Ring a => a -> Integer -> a
 expo x 0 = mulId
 expo x n = mul x (expo x (n-1))
 
@@ -163,37 +169,11 @@ eval p x = foldl (\acc -> \ele -> add acc ele) addId $ [i | let deg = degree p,
                                                             let ls' = (\pow -> mul (coeff p pow) (expo x pow)) <$> ls,
                                                             i <- ls']
 
-
-sub :: Group a => a -> a -> a
-sub p1 p2 = add p1 (addInv p2)
-
-examplePoly1 = Poly (Map.fromList [(2, 3), (1, -1), (0, 2)]) :: Poly Int
-examplePoly2 = Poly (Map.fromList [(2, 2), (1, 5), (0, -10)]) :: Poly Int
-
-p = Poly (Map.fromList [(3, 1), (2, -3), (1,2)]) :: Poly Int
-t = Poly (Map.fromList [(2, 1), (1, -3), (0,2)]) :: Poly Int
-h = Poly (Map.fromList [(1,1)]) :: Poly Int
-
-evalDistributesOverMul :: (Eq r, Ring r) => r -> Poly r -> Poly r -> Bool
+evalDistributesOverMul :: forall r. (Eq r, Ring r) => r -> Poly r -> Poly r -> Bool
 evalDistributesOverMul r p1 p2 = mul (eval p1 r) (eval p2 r) == eval (mul p1 p2) r 
 
 
-
--- Verifier side
-r = 23 :: Int
-rAtt = eval t r
-
--- transmit r from verifier -> prover
-
--- Prover side
-rAtp = eval p r
-rAth = eval h r
-
--- transmit rAtp and rAth from prover -> verifier
-check = rAtt * rAth == rAtp
-
-
-
+------------------------------------ Modular Arithmetic -------------------------
 
 newtype ModInt = ModInt Integer
   deriving (Show, Eq)
@@ -203,7 +183,7 @@ instance Arbitrary ModInt where
     i <- arbitrary
     return (modInt i)
 
-ourPrime = 1046527
+ourPrime = 7
 
 modInt :: Integer -> ModInt
 modInt i = ModInt (i `mod` ourPrime)
@@ -241,13 +221,24 @@ instance Field ModInt where
     where
       x = fst $ extendedEuclid i ourPrime
 
+raise :: ModInt -> Integer -> ModInt
+raise b p
+  | p < 0     = raise (mulInv b) (-p)
+  | p == 0    = mulId
+  | otherwise = mul b $ raise b (p - 1)
+
+testRaise :: ModInt -> Integer -> Bool
+testRaise b p = if (b /= addId) then mul (raise b p) (raise b (-p)) == mulId else True
+
+---------------------- "Homomorphic Encryption" -----------------------------------
 
 ourG = 5
+
 newtype EncModInt = EncModInt ModInt
   deriving (Show, Eq)
 
 encModInt :: Integer -> EncModInt
-encModInt i = EncModInt $ modInt $ ourG^i
+encModInt i = EncModInt $ raise (modInt ourG) i
 
 instance Arbitrary EncModInt where
   arbitrary = do
@@ -258,36 +249,29 @@ instance Monoid EncModInt where
   add (EncModInt n1) (EncModInt n2) = EncModInt (mul n1 n2)
   addId = EncModInt $ mulId
 
--- instance Group EncModInt where
---   addInv (EncModInt n) = 
+instance Group EncModInt where
+  addInv (EncModInt n) = EncModInt (mulInv n)
 
--- 3x^2 - x + 2
--- 2x^2 + 5x -10
+encMul :: EncModInt -> Integer -> EncModInt
+encMul (EncModInt b) p = EncModInt $ raise b p
 
--- 3x^2 - x + 2 = 2x^2 + 5x -10
--- a3x^3 + a2x^2 ...
--- (x + a) (x + b) (x + c) 
+testEncAdd :: Integer -> Integer -> Bool
+testEncAdd i1 i2 = encModInt (i1 + i2) == add (encModInt i1) (encModInt i2)
 
--- (3x^2 - x + 2) * (2x^2 + 5x -10) =
--- 6x^4   -2x^3 + 4x^2
--- 15x^3  -5x^2 + 10x
--- -30x^2 +10x  - 20
+testEncMul :: Integer -> Integer -> Bool
+testEncMul i1 i2 = encMul (encModInt i1) i2 == encModInt (i1 * i2)
 
--- (4,6), (3, 13), (2, -31), (1, 20), (0, -20)
 
--- fix :: (a -> a) -> a
--- fix f = x
---   where
---     x = f x
-
--- fibeeee = fix (\fib -> 0:1:(zipWith (+) fib (tail fib)))
-
+-------------------------- Main -------------------------------------------
 
 main :: IO ()
 main = do
-  isCommutativeRing @Int
-  isCommutativeRing @(Poly Int)
-  isField           @ModInt
+  isCommutativeRing @Integer
+  isCommutativeRing @(Poly Integer)
   isCommutativeRing @(Poly ModInt)
-
+  isField @ModInt
+  quickCheck $ evalDistributesOverMul @Integer  
+  quickCheck $ testRaise
+  quickCheck $ testEncAdd
+  quickCheck $ testEncMul
 
